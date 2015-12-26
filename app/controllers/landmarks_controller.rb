a4917196cffbb9113a82bf1a902874c332ca8f16
jakebrady5 <jakebrady5@gmail.com>
2015-12-26 10:59:12 -0500

    general setup

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,22 @@
 class LandmarksController < ApplicationController
 
+  get '/landmarks' do
+  end
+
+  get '/landmarks/new' do
+    erb :new
+  end
+
+  post '/landmarks/new' do 
+  end
+
+  get '/landmarks/:id' do
+  end
+
+  get '/landmarks/:id/edit' do
+  end
+
+  post '/landmarks/:id/edit' do
+  end
+
 end
